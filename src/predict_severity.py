7550a9e5e9145ef572ba1aa585d8acf7c7401be8
JimKarpodinis <dkarpodinis2@gmail.com>
2025-05-07 08:14:10 +0000

    feat: Implement severiy prediction script

---
+++ src/predict_severity.py
@@ -0,0 +1,79 @@
+import argparse
+import os
+from transformers import AutoModel, pipeline
+from accelerate import PartialState
+from accelerate.utils import gather_object
+from datasets import Dataset, load_dataset
+import torch
+
+
+def main(model_name: str, data_dir: str) -> None:
+
+    hf_token = os.getenv("HF_TOKEN")
+
+    model = pipeline(
+            "text-generation", model=model_name, device_map="auto",
+            token=hf_token, return_full_text=False)
+
+    dataset = load_dataset("csv", data_dir=data_dir, split="train")
+
+    dataset = dataset.map(lambda example: create_instruction_prompt(
+        example, target_language))
+
+    dataset = dataset.map(lambda batch: translate_sentences(
+        batch, model), batched=True, batch_size=64)
+
+    save_dataset(dataset, data_dir, model_name)
+
+
+def create_instruction_prompt(example: dict, target_language: str) -> dict:
+
+    messages = [
+        {"role": "system",
+        "content": f"""Predict the level of depression in the following sentences.
+            Your response should contain only one word.
+            The possible depression severity labels are: 'Minimum', 'Mild', 'Moderate', 'Severe'.
+            Use one and only one of these words to classify each sentence."""},
+        {"role": "user", "content": example["translated_text"]},
+    ]
+
+    example["instruction_prompt"] = messages
+
+    return example
+
+
+def predict_severity(examples: dict, model: AutoModel) -> dict:
+
+    model_outputs = model(
+            examples["instruction_prompt"],
+            max_new_tokens=1)
+
+    preds = [output[0]["generated_text"] for output in model_outputs]
+
+    examples["depression_severity"] = preds
+
+    return examples
+
+
+def save_dataset(dataset: Dataset, data_dir: str, model_name: str): 
+
+    dataset_name = os.path.basename(data_dir)
+    model_name = model_name.split("/")[-1]
+
+    dataset_name += f"_{model_name}_severity_prediction"
+
+    dataset.to_csv(f"data/processed/{dataset_name}.csv")
+
+
+if __name__ == "__main__":
+    
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument("--model_name", help= "The hf model name", type=str)
+  parser.add_argument("--data_dir", help= "The path where the data reside", type=str)
+
+  args = vars(parser.parse_args())
+  # dict of argument
+
+  main(**args)
+
